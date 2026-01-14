a3622838db5fa270dcc1a7880e7067b083830a23
Maicol Freire <maicolfreire001@gmail.com>
2026-01-14 02:31:19 -0300

    Migrating to render

--- bot.py
+++ bot.py
@@ -3,11 +3,28 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
+from flask import Flask
+from threading import Thread
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 APP_ID = os.getenv("APP_ID")
 GUILD_ID = os.getenv("GUILD_ID")
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+# -------------------------------------------
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -36,4 +53,5 @@ if __name__ == "__main__":
     if not TOKEN:
         print("DISCORD_TOKEN not found in .env")
     else:
+        keep_alive()
         bot.run(TOKEN)
